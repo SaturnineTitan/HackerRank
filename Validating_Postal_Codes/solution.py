95bb6e030b902fa4f250b12d54499ee20d27f329
T.A. Truesdell <43506369+tatruesdell@users.noreply.github.com>
2022-08-25 14:46:10 -0400

    Update solution.py

--- Validating_Postal_Codes/solution.py
+++ Validating_Postal_Codes/solution.py
@@ -1,4 +1,4 @@
-regex_integer_in_range = r"[100000-999999]"	# Check range of given integer 
+regex_integer_in_range = r"^[1-9][\d]{5}$"	# Check range of given integer 
 
 regex_alternating_repetitive_digit_pair = r"(?=(\d)\d\1)" # solution 1
 
