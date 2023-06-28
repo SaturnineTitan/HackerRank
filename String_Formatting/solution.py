7f7f7f70c2e4f7e92201b445b6e52ea539a41827
T.A. Truesdell <43506369+tatruesdell@users.noreply.github.com>
2023-06-28 17:40:52 -0400

    Delete String_Formatting directory

--- String_Formatting/solution.py
+++
@@ -1,6 +0,0 @@
-def print_formatted(number):
-    # your code goes here
-    pad = len(bin(number)[2:])
-    for i in range(1, number + 1):
-        print('{:>{width}d} {:>{width}o} {:>{width}X} {:>{width}b}'.format(i, i, i, i, width=pad))
-    return
