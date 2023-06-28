d506bfe2d83714804f7d828064c88a2ff9deca74
T.A. Truesdell <43506369+tatruesdell@users.noreply.github.com>
2023-06-28 17:31:05 -0400

    Delete Polynomials directory

--- Polynomials/solution.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-# Initialize coefficient list from input 
-coefficients = numpy.array(list(map(float, input().split())))
-# independent variable 
-x = float(input())
-# Evaluate function at x from given coefficients 
-print(numpy.polyval(coefficients, x))
