185c2985a9abd71895b9b94bd313d1afa9f77dd2
T.A. Truesdell <43506369+tatruesdell@users.noreply.github.com>
2023-06-28 17:25:50 -0400

    Delete Inner_and_Outer_Product directory

--- Inner_and_Outer_Product/solution.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-# Convert input from string to list (array) of integers 
-A = numpy.array(list(map(int, input().split())))
-B = numpy.array(list(map(int, input().split())))
-# print inner and outer products 
-print(numpy.inner(A, B))
-print(numpy.outer(A, B))
