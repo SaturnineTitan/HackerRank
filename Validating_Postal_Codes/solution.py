c5fdfb309f46c408910f137c472f51cb589e9f4c
T.A. Truesdell <43506369+tatruesdell@users.noreply.github.com>
2023-06-28 17:47:17 -0400

    Delete Validating_Postal_Codes directory

--- Validating_Postal_Codes/solution.py
+++
@@ -1,5 +0,0 @@
-regex_integer_in_range = r"^[1-9][\d]{5}$"	# Check range of given integer 
-
-regex_alternating_repetitive_digit_pair = r"(?=(\d)\d\1)" # solution 1
-
-regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)" # solution 2
